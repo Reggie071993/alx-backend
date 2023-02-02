704832fdf033b08c3b870fe933fff15a3ca7b0c3
Reggie071993 <reykakwela@yahoo.com>
2023-02-02 10:17:29 +0000

    3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
-"""
-Parametrize templates
-"""
+'''
+    Use Babel to get user locale.
+'''
 
-import babel
-from flask import Flask, render_template, request
 from flask_babel import Babel
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
-class Config:
-    """
-    Config class
-    """
+class Config(object):
+    '''
+        Babel configuration.
+    '''
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -23,21 +23,21 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localeselector
-def get_locale():
-    """
-     determine the best match with our supported languages.
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """
-    hello world
-    """
+def helloWorld() -> str:
+    '''
+        Render template for Babel usage.
+    '''
     return render_template('3-index.html')
 
 
+@babel.localeselector
+def get_locale() -> str:
+    '''
+        Get user locale to serve matching translation.
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
